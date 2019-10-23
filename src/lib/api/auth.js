98a286fa378731b4a5f4e1eab13670782ef47aba
daeun <wjdekdms99@naver.com>
2019-10-23 18:37:55 +0900

    add proxy server

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export const checkEmailExists = (email) => axios.get('http://localhost:8080/api/member/email/?email=' + email);
-export const checkIdExists = (id) => axios.get('http://localhost:8080/api/member/id/?id=' + id);
-export const checkPhoneExists = (phone) => axios.get('http://localhost:8080/api/member/phone/?phone=' + phone);
+export const checkEmailExists = (email) => axios.get('/api/member/email/?email=' + email);
+export const checkIdExists = (id) => axios.get('/api/member/id/?id=' + id);
+export const checkPhoneExists = (phone) => axios.get('/api/member/phone/?phone=' + phone);
 
-export const localRegister = ({email,id,password,phone,birthday,name,comment,address,gender}) => axios.post('http://localhost:8080/api/member', {email,id,password,phone,birthday,name,comment,address,gender});
-export const localLogin = ({id,password}) => axios.post('http://localhost:8080/api/member/auth', {id, password},{withCredentials: true});
-export const localRegisterImage = (formData) => axios.post('http://localhost:8080/api/member/image',formData,{headers:{'content-type' : 'multipart/form-data',withCredentials: true}});
-export const checkStatus = () => axios.get('http://localhost:8080/api/member/auth/token/id', {withCredentials: true});
-export const logout = () => axios.get('http://localhost:8080/api//member/auth/token', {withCredentials: true});
+export const localRegister = ({email,id,password,phone,birthday,name,comment,address,gender}) => axios.post('/api/member', {email,id,password,phone,birthday,name,comment,address,gender});
+export const localLogin = ({id,password}) => axios.post('/api/member/auth', {id, password},{withCredentials: true});
+export const localRegisterImage = (formData) => axios.post('/api/member/image',formData,{headers:{'content-type' : 'multipart/form-data',withCredentials: true}});
+export const checkStatus = () => axios.get('/api/member/auth/token/id', {withCredentials: true});
+export const logout = () => axios.get('/api/member/auth/token', {withCredentials: true});
--- src/lib/api/users.js
+++ src/lib/api/users.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
-export const getUserInfo = () => axios.get('http://localhost:8080/api/member/info',{withCredentials: true});
-export const checkUserAndGetInfo = () => axios.get('http://localhost:8080/api/auth',{withCredentials: true});
+export const getUserInfo = () => axios.get('/api/member/info',{withCredentials: true});
+export const checkUserAndGetInfo = () => axios.get('/api/auth',{withCredentials: true});
